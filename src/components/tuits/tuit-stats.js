ee096ed185cd13eaf54a53433d572c59aa78302b
LAPTOP-U8I5ON0L\shrey <shreyajain11197@gmail.com>
2022-04-02 18:56:20 -0400

    changes for dislikes

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+import {findAllTuitsLikedByUser} from "../../services/likes-service";
+import {findAllTuitsDislikedByUser} from "../../services/dislikes-service";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({tuit, likeTuit = () => {}, dislikeTuit = () => {}}) => {
+    const [checkIfTuitLiked, setCheckIfTuitLiked] = useState(false);
+    const [checkIfTuitIsDisliked, setCheckIfTuitIsDisliked] = useState(false);
+    findAllTuitsLikedByUser("me").then(response => setCheckIfTuitLiked(
+        (response.filter(tuitFetchedFromDb => tuitFetchedFromDb._id === tuit._id)).length > 0));
+    findAllTuitsDislikedByUser("me").then(response => setCheckIfTuitIsDisliked(
+        (response.filter(tuitFetchedFromDb => tuitFetchedFromDb._id === tuit._id)).length > 0));
     return (
       <div className="row mt-2">
         <div className="col">
@@ -14,14 +22,27 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
         <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
-                tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
+                  checkIfTuitLiked === true &&
                   <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
               }
               {
-                tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                  checkIfTuitLiked === false &&
                   <i className="far fa-heart me-1"></i>
               }
             {tuit.stats && tuit.stats.likes}
+          </span>
+        </div>
+        <div className="col">
+          <span onClick={() => dislikeTuit(tuit)}>
+              {
+                  checkIfTuitIsDisliked === true &&
+                  <i className="fas fa-thumbs-down me-1" style={{color: 'red'}}></i>
+              }
+              {
+                  checkIfTuitIsDisliked === false &&
+                  <i className="far fa-thumbs-down me-1"></i>
+              }
+              {tuit.stats && tuit.stats.dislikes}
           </span>
         </div>
         <div className="col">
